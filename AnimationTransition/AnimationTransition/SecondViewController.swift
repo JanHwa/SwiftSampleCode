664196d57a638df86a6c91f5f46d28925ab9133a
Justin <jianhuatung@gmail.com>
2017-07-09 16:46:46 +0800

    添加转场动画

---
+++ AnimationTransition/AnimationTransition/SecondViewController.swift
@@ -0,0 +1,40 @@
+//
+//  SecondViewController.swift
+//  AnimationTransition
+//
+//  Created by Justin on 2017/7/9.
+//  Copyright © 2017年 Justin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var dismissButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        dismissButton.layer.cornerRadius = dismissButton.frame.size.width / 2
+    }
+    @IBAction func dismissButton(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ AnimationTransition/AnimationTransition/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  AnimationTransition
+//
+//  Created by Justin on 2017/7/9.
+//  Copyright © 2017年 Justin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController,UIViewControllerTransitioningDelegate {
+
+    @IBOutlet weak var presentButton: UIButton!
+    
+    let transition = CircularTransition()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presentButton.layer.cornerRadius = presentButton.frame.size.width / 2
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let secondVC = segue.destination as! SecondViewController
+        secondVC.transitioningDelegate = self
+        secondVC.modalPresentationStyle = .custom
+    }
+    
+    func animationController(forPresented presented: UIViewController, presenting: UIViewController, source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        
+        transition.transitionMode = .present
+        transition.startingPoint = presentButton.center
+        transition.circleColor = presentButton.backgroundColor!
+        return transition
+    }
+    
+    func animationController(forDismissed dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transition.transitionMode = .dismiss
+        transition.startingPoint = presentButton.center
+        transition.circleColor = presentButton.backgroundColor!
+        return transition
+    }
+}
+
