5061a65f39961fe2afe7ad001754035f2aa501e9
HWA <jianhuatung@gmail.com>
2018-08-10 18:01:33 +0800

    更新RxSwift

---
+++ RxSwiftDemo/RxSwiftDemo/BaseViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BaseViewController.swift
+//  RxSwiftDemo
+//
+//  Created by HWA on 2018/8/10.
+//  Copyright © 2018年 Justin. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+
+class BaseViewController: UIViewController {
+
+    var disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+ 
+}
